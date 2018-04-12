cbef5758525f86d986d0cf11775ecaceba9a9fe6
chunyan at home <emilyusa@yahoo.com>
2018-04-12 14:11:11 -0700

    create work.js

---
+++ Week02-JavaScriptObjects/work.js
@@ -0,0 +1,27 @@
+function divider(title){
+	console.log("===================");
+    console.log(title);
+    console.log("===================");
+	}
+
+var MyObject1={
+	firstName: "George",
+	lastName:"Washington"
+	};
+
+divider("Person")
+console.log(MyObject1.firstName);
+console.log(MyObject1.lastName);
+console.log(MyObject1.firstName+" "+MyObject1.lastName);
+
+var MyObject2={
+	operand01: 6,
+	operand02: 10
+	};
+
+divider("Caculator");
+console.log("operand01="+MyObject2.operand01);
+console.log("operand02="+MyObject2.operand02);
+console.log("Add: "+(MyObject2.operand01+MyObject2.operand02));
+console.log("Subtract: "+(MyObject2.operand01-MyObject2.operand02));
+console.log("Multiply: "+(MyObject2.operand01*MyObject2.operand02));
