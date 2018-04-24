b6bd5f40eb9815d031b8eb1b1e503e22bb244868
chunyan at home <emilyusa@yahoo.com>
2018-04-24 11:01:27 -0700

    add week04-prop-basics directory

--- week03-react-jest/src/index.js
+++ week03-react-jest/src/index.js
@@ -4,5 +4,16 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const myProps = {
+    testProp: 2,
+    myProp:3
+};
+
+ReactDOM.render(
+    <App myProps={myProps} />,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
