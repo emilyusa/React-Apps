29b36e970b25e1032771ee2172127616b004e243
chunyan at home <emilyusa@yahoo.com>
2018-05-25 10:24:02 -0700

    completed AddressProxy with all testing

---
+++ AddressProxy/src/components/Address.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+import tempAddressList from '../address-list';
+import AddressShow from './AddressShow';
+
+class Address extends Component {
+
+    constructor(props) {
+        super(props);
+        this.addressList = null;
+        this.addressIndex = 0;
+        this.state = {
+            address: tempAddressList[this.addressIndex]
+        };
+        this.getAddress();
+    }
+
+    getAddress=()=> {
+        fetch('/address-list')
+            .then((response) => response.json())
+            .then((addressListFromServer) => {
+                this.addressList=addressListFromServer;
+            })
+            .catch((ex) => {
+                console.log(ex);
+            })
+    }
+
+    setAddress=(offset)=>{
+        if(offset==0){
+            const middlebutton=tempAddressList[1];
+            this.setState({
+                address:middlebutton
+            });
+        }else {
+            this.addressIndex += offset;
+            if (this.addressIndex > this.addressList.result.length - 1) {
+                this.addressIndex = 0;
+            }
+            else if (this.addressIndex < 0) {
+                this.addressIndex = this.addressList.result.length - 1;
+            }
+            this.setState({
+                address: this.addressList.result[this.addressIndex]
+            });
+        }
+    };
+
+
+    render() {
+        if (!this.quiet){console.log("ADDRESS RENDER");}
+        return (
+            <div>
+                <AddressShow
+                    address={this.state.address}
+                    setAddress={this.setAddress}
+                />
+            </div>
+
+        );
+    }
+}
+
+export default Address;
