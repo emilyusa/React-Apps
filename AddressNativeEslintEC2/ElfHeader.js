dea5754a10ac6660086918eb83af47b9d3396c44
chunyan at home <emilyusa@yahoo.com>
2018-05-26 16:44:01 -0700

    move midterm to EC2 for AddressProxyEslintEC2 and AddressNativeEslintEC2

---
+++ AddressNativeEslintEC2/ElfHeader.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-native';
+import { Text, View } from 'react-native';
+import styles from './elf-styles';
+
+export default class ElfHeader extends React.Component {
+    render() {
+        return (
+            <View style={styles.nav}>
+                <Link to="/" underlayColor="#f0f4f7" style={styles.navItem}>
+                    <Text>Address</Text>
+                </Link>
+                <Link
+                    to="/get-file"
+                    underlayColor="#f0f4f7"
+                    style={styles.navItem}
+                >
+                    <Text>GetFile</Text>
+                </Link>
+            </View>
+        );
+    }
+}
---
+++ AddressProxyEslintEC2/src/test/App.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from '../components/App';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+configure({ adapter: new Adapter() });
+
+describe('App test', function() {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(
+            <MuiThemeProvider>
+                <App />
+            </MuiThemeProvider>,
+            div
+        );
+        ReactDOM.unmountComponentAtNode(div);
+    });
+});
